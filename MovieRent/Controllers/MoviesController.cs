e72610a6aa4be0d31471deaccdd0fcb9930d7d69
Ermin <erminsrna17@gmail.com>
2020-10-21 21:14:07 +0200

    Add project files.

---
+++ MovieRent/Controllers/MoviesController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MovieRent.Models;
+using MovieRent.ViewModels;
+namespace MovieRent.Controllers
+{
+    public class MoviesController : Controller
+    {
+
+        // GET: Movies
+        public ActionResult Random()
+        {
+            var movie = new Movie(){ Name = "Shrek!" };
+
+            var customers = new List<Customer>
+            {
+                new Customer {Name = "Customer 1"},
+                new Customer {Name = "Customer 2"}
+            };
+
+            var viewModel = new RandomMovieViewModel
+            {
+                Movie = movie,
+                Customers = customers
+            };
+                
+              
+
+            return View(viewModel);
+            
+
+        }
+
+
+    }
+}
---
+++ MovieRent/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MovieRent.MvcApplication" Language="C#" %>
